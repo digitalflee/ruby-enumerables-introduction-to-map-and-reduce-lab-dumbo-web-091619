b8838c69e0e5b501fa9bc189f29d3cd748207ec1
Keenan Alves <keenan.alves@gmail.com>
2019-09-02 00:53:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,3 +18,10 @@ def reduce_to_total(source_array, starting_point)
   source_array.reduce(starting_point) { |sum, num| sum + num }
 end
 
+def reduce_to_all_true(source_array)
+  source_array.reduce{ |sum, num| sum + num }
+end
+
+def reduce_to_any_true(source_array)
+  source_array.reduce{ |sum, num| sum + num }
+end
