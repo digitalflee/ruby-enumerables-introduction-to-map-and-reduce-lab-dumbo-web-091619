15e73b8cb4a818901bad83d20c6073336c6757b0
Keenan Alves <keenan.alves@gmail.com>
2019-09-02 00:11:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,4 @@
 def map_to_negativize(source_array)
-  
-  puts negative_array << map(source_array, -1)
-  return negative_array 
+  puts negative_array = []
+  negative_array << map(source_array, -1)
 end
