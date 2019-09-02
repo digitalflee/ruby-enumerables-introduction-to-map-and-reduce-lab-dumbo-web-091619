d3751532d3eee5b9eb48e24814e2ee9ee27e3a19
Keenan Alves <keenan.alves@gmail.com>
2019-09-02 01:05:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,8 +15,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  puts starting_point == 0
-  source_array.reduce() { |sum, n| sum + n }
+  source_array.reduce{ |sum, n| sum + n }
 end
 
 def reduce_to_all_true(source_array)
